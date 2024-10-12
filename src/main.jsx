2aae13799197220dca5b91c1c5861ad5c1249455
Arunpandiyan <73156415+Arunpandiyan3620@users.noreply.github.com>
2024-10-12 21:37:46 +0530

    path changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
